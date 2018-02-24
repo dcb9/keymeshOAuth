12885e180f1e6b608df2384762d68f7eaf67d14d
dcb9 <bob@phpor.me>
2018-02-24 16:45:46 +0800

    add structure

--- main.go
+++ main.go
@@ -9,6 +9,13 @@ import (
 
 func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 
+	switch request.Path {
+	case "/twitter/login-url":
+		return getTwitterLoginURL()
+	case "/twitter/user-info":
+		return getTwitterUserInfo()
+	}
+
 	index, err := ioutil.ReadFile("public/index.html")
 	if err != nil {
 		return events.APIGatewayProxyResponse{}, err
@@ -26,4 +33,18 @@ func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRespo
 
 func main() {
 	lambda.Start(handler)
+}
+
+func getTwitterLoginURL() (events.APIGatewayProxyResponse, error) {
+	return events.APIGatewayProxyResponse{
+		Body:       "http://fake.twitter.login.url",
+		StatusCode: 200,
+	}, nil
+}
+
+func getTwitterUserInfo() (events.APIGatewayProxyResponse, error) {
+	return events.APIGatewayProxyResponse{
+		Body:       `{"foo": "bar"}`,
+		StatusCode: 200,
+	}, nil
 }
